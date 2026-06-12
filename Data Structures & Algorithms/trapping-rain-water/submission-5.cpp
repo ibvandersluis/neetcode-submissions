51170dd1ff6bcaec20b2def70483dd4232349b1a
Isaac van der Sluis <isaacvandersluis@gmail.com>
2026-06-12 11:53:15 +0100

    Add: trapping-rain-water - submission-5

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-5.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        // Multi-array
+
+        vector<int> leftMax(height.size());
+        vector<int> rightMax(height.size());
+
+        int currMaxL = height[0];
+        for (int i = 0; i < height.size(); ++i) {
+            currMaxL = max(height[i], currMaxL);
+            leftMax[i] = currMaxL;
+        }
+
+        int currMaxR = height[height.size() - 1];
+        for (int i = height.size() - 1; i > 0; --i) {
+            currMaxR = max(height[i], currMaxR);
+            rightMax[i] = currMaxR;
+        }
+
+        int totalWater = 0;
+        for (int i = 0; i < height.size() - 1; ++i) {
+            totalWater += max(0, min(leftMax[i], rightMax[i]) - height[i]);
+        }
+
+        return totalWater;
+    }
+};
